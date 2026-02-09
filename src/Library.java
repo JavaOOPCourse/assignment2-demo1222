7166e6542e67f161e513c4c067851e2d462ef526
Zarylbek <Democtras05@gmail.com>
2026-02-09 18:48:23 +0600

    Refactor Library methods and remove TODO comments

--- src/Library.java
+++ src/Library.java
@@ -3,33 +3,69 @@ public class Library {
     private Book[] books;
     private int count;
 
+    // Constructor
     public Library(int capacity) {
         books = new Book[capacity];
         count = 0;
     }
 
-    // TODO: Add book to array
+    // Add book to array
     public void addBook(Book book) {
-        // implement
+        if (count < books.length) {
+            books[count] = book;
+            count++;
+        } else {
+            System.out.println("Library is full. Cannot add more books.");
+        }
     }
 
-    // TODO: Display all books
+    // Display all books
     public void displayBooks() {
-        // implement
+        if (count == 0) {
+            System.out.println("No books in the library.");
+            return;
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(books[i]);
+        }
     }
 
-    // TODO: Search book by title
+    // Search book by title
     public Book searchByTitle(String title) {
+        for (int i = 0; i < count; i++) {
+            if (books[i].getTitle().equalsIgnoreCase(title)) {
+                return books[i];
+            }
+        }
         return null;
     }
 
-    // TODO: Borrow book by title
+    // Borrow book by title
     public void borrowBook(String title) {
-        // implement
+        Book book = searchByTitle(title);
+
+        if (book == null) {
+            System.out.println("Book not found.");
+        } else if (!book.isAvailable()) {
+            System.out.println("Book is already borrowed.");
+        } else {
+            book.borrowBook();
+            System.out.println("Book borrowed successfully.");
+        }
     }
 
-    // TODO: Return book by title
+    // Return book by title
     public void returnBook(String title) {
-        // implement
+        Book book = searchByTitle(title);
+
+        if (book == null) {
+            System.out.println("Book not found.");
+        } else if (book.isAvailable()) {
+            System.out.println("Book was not borrowed.");
+        } else {
+            book.returnBook();
+            System.out.println("Book returned successfully.");
+        }
     }
 }
